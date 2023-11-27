3680840d5a0f7f300712eac83101fff8ba650ea2
Tinh Ngo <tinhngo.ptho@gmail.com>
2023-11-27 20:39:11 +0700

    Update All

--- main.cpp
+++ main.cpp
@@ -6,15 +6,19 @@ using namespace std;
 #define ull unsigned long long
 #define fi first
 #define se second
+#define ii pair<int, int>
 #define vt vector
 #define pub push_back
-#define ii pair<int, int>
 #define all(s) (s).begin(), (s).end()
 #define sz(s) (int)(s).size()
-#define REP(i, n)  for (int i = 0, _n = (n); i < _n; ++i)
-#define FOR(i, a, b)  for (int i = (a), _b = (b); i < _b; ++i)
-#define FORD(i, b, a)  for (int i = (b), _a = (a); i >= _a; --i)
-#define FORE(i, a, b)  for (int i = (a), _b = (b); i <= _b; ++i)
+#define sqr(x) (x) * (x)
+#define reset(f, v) memset(f, v, sizeof f)
+#define FORC(i, a, b, s) for (__typeof(a) i = (a), _b = (b); (s)>0 ? i<_b : i>_b; i += (s))
+#define FORCE(i, a, b, s) FORC(i, a, (s > 0) ? b+1 : b-1, s)
+#define FOR(i, a, b) FORC(i, a, b, 1)
+#define FORE(i, a, b) FORCE(i, a, b, 1)
+#define FORD(i, b, a) FORC(i, a, b, -1)
+#define FORDE(i, b, a) FORCE(i, b, a, -1)
 #define file(name) if (fopen(name".inp", "r")) { freopen(name".inp", "r", stdin); freopen(name".out", "w", stdout); }
 
 template<class T> bool umin(T& a, const T& b) {
@@ -29,12 +33,9 @@ const int INF = 1e9 + 1e8 + 1e7;
 const int MOD = 1e9 + 7;
 const int mxN  = 1e6 + 11;
 
-int n;
 
 signed main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-	
-	|
-	
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        |
 }
