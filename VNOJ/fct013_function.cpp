dc9fc7b2e52b2c7c7604188653242b174658e240
Tinh Ngo <tinhngo.ptho@gmail.com>
2023-12-06 17:04:18 +0700

    ADD 6/12/2023

---
+++ VNOJ/fct013_function.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define NAME ""
+#define endl '\n'
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define all(s) (s).begin(), (s).end()
+#define sz(s) (int)(s).size()
+#define sqr(x) (ll) (x) * (x)
+#define FOR(i, a, b) for (__typeof(a) i = (a); i <= (b); ++i)
+#define FORD(i, b, a) for (__typeof(b) i = (b); i >= (a); --i)
+#define FORC(i, a, b, s) for (__typeof(a) i = (a); i <= (b); i += (s))
+
+template <class T> bool umin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template <class T> bool umax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+
+signed main(void) {
+        cin.tie(0), cout.tie(0) -> sync_with_stdio(0);
+        if (fopen(NAME ".inp", "r"))
+                freopen(NAME ".inp", "r", stdin), freopen(NAME ".out", "w", stdout);
+
+        ll n;
+        cin >> n;
+        cout << n / 2 - (n & 1 ? n : 0);
+}
+
+/**-----------------------------------------
+-------------Author: tinhnopro -------------
+-----------From: NQB with luv <3 -----------
+---------//--------NVT---------//-----------
+-----------------------------------------**/
