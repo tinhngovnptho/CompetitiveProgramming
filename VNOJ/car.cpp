9f76479041b2262698a0c398274476bc167bb1e2
Tinh Ngo <tinhngo.ptho@gmail.com>
2023-12-07 22:06:04 +0700

    ADD 7/12/2023

--- VNOJ/car.cpp
+++ VNOJ/car.cpp
@@ -20,13 +20,41 @@ using namespace std;
 template <class T> bool umin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
 template <class T> bool umax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
 
+struct car
+{
+	int a, b, idx;
+};
+
+car v[100000+11];
+int n;
+
+bool cmp(const car &a, const car &b)
+{
+	return 1LL * a.a * b.b > 1LL * b.a * a.b;
+}
 
 signed main()
 {
 	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 	file("");
 
-	|
+	cin >> n;
+	ForE(i, 1, n) cin >> v[i].a;
+	ForE(i, 1, n)
+	{
+		cin >> v[i].b;
+		v[i].idx = i;
+	}
+	sort(v+1, v+n+1, cmp);
+//	ForE(i, 1, n) cerr << v[i].a << " " << v[i].b << "\n";
+	ll day = 0, ans = 0;
+	ForE(i, 1, n)
+	{
+		day += v[i].b;
+		ans += v[i].a * day;
+	}
+	cout << ans << endl;
+	ForE(i, 1, n) cout << v[i].idx << " ";
 }
 
 /**-----------------------------------------
