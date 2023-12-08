613da51d7fe850a11b2684a577dbfd7b603838eb
Tinh Ngo <tinhngo.ptho@gmail.com>
2023-12-08 21:45:17 +0700

    ADD 8/12/2023

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define NVT ""
 #define endl '\n'
 #define fi first
 #define se second
@@ -12,24 +13,33 @@ using namespace std;
 #define sqr(x) (ll) (x) * (x)
 #define ALL(s) (s).begin(), (s).end()
 #define SZ(s) (int)(s).size()
-#define For(i, a, b) for (__typeof(a) i = (a); i < (b); ++i)
-#define ForE(i, a, b) For(i, a, b+1)
-#define ForD(i, b, a) for (__typeof(b) i = (b); i >= (a); --i)
-#define file(NAME) if (fopen(NAME".inp", "r")) freopen(NAME".inp", "r", stdin), freopen(NAME".out", "w", stdout);
+#define For(i, a, b) for (__typeof(a) i = (a), _b = (b); i < _b; ++i)
+#define ForE(i, a, b) for (__typeof(a) i = (a), _b = (b); i <= _b; ++i)
+#define ForD(i, b, a) for (__typeof(b) i = (b), _a = (a); i >= _a; --i)
 
 template <class T> bool umin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
 template <class T> bool umax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
 
+const ll oo = 1e18+11;
+const int MOD = 1e9+7;
+const int mxN = 2e5+7;
+const int mxVal = 1e6+11;
+
 
 signed main(void) {
-        ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-        file("");
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	if (fopen(NVT ".inp", "r")) {
+		freopen(NVT ".inp", "r", stdin);
+		freopen(NVT ".out", "w", stdout);
+	}
 
-
+	|
 }
 
 /**-----------------------------------------
--------------Author: tinhnopro -------------
------------While(!Die) Code(); <3 ----------
----------//--------NVT---------//-----------
------------------------------------------**/
+---------- Author: tinhnopro ---------------
+----------While(!Die) Code();---------------
+---------//-------NVT-------//--- /\_/\ ----
+-------------------------------- (= ._.) ---
+-------------------------------- />WA  \> **/
