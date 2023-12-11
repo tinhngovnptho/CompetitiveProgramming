154ca052b68ed6068c0d8234681e6da1ea36f409
Tinh Ngo <tinhngo.ptho@gmail.com>
2023-12-11 22:08:35 +0700

    ADD 11/12/2023

--- main.cpp
+++ main.cpp
@@ -1,36 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define NAME ""
-#define endl '\n'
-#define fi first
-#define se second
 #define ll long long
 #define ld long double
 #define ull unsigned long long
-#define pii pair<int, int>
-#define pll pair<ll, ll>
+#define fi first
+#define se second
 #define sqr(x) (ll) (x) * (x)
-#define ALL(s) (s).begin(), (s).end()
-#define SZ(s) (int)(s).size()
-#define For(i, a, b) for (__typeof(a) i = (a), _b = (b); i < _b; ++i)
-#define ForE(i, a, b) for (__typeof(a) i = (a), _b = (b); i <= _b; ++i)
-#define ForD(i, b, a) for (__typeof(b) i = (b), _a = (a); i >= _a; --i)
+#define all(s) (s).begin(), (s).end()
+#define sz(s) (int)(s).size()
+#define FOR(i, a, b) for (__typeof(a) i = (a); i < (b); ++i)
+#define FORE(i, a, b) for (__typeof(a) i = (a); i <= (b); ++i)
+#define FORD(i, b, a) for (__typeof(b) i = (b); i >= (a); --i)
 
 template <class T> bool umin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
 template <class T> bool umax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
-
-const ll oo = 1e18+11;
-const int MOD = 1e9+7;
-const int mxN = 2e5+7;
-const int mxVal = 1e6+11;
 
 
 signed main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
-	// freopen(NAME ".inp", "r", stdin);
-	// freopen(NAME ".out", "w", stdout);
+	if (fopen("NVT.inp", "r")) {
+		assert(freopen("NVT.inp", "r", stdin));
+		assert(freopen("NVT.out", "w", stdout));
+	}
 	|
 }
 
