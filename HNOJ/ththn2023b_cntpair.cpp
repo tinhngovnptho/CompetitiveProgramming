62518df5a9132a9baa2cf635faa557065aa40846
Tinh Ngo <tinhngo.ptho@gmail.com>
2023-12-19 21:39:17 +0700

    ADD19/12/2023

--- HNOJ/ththn2023b_cntpair.cpp
+++ HNOJ/ththn2023b_cntpair.cpp
@@ -16,49 +16,20 @@ template <class X, class Y> bool umax(X &a, const Y &b) { return a < b ? a = b, 
 template <class T> string to_str(const T &a, int p = -1) { stringstream ss; p >= 0 ? ss << fixed << setprecision(p) << a : ss << a; return ss.str(); }
 template <class T> T abs(const T &a) { return a >= 0 ? a : -a; }
 
-#define MAX_N	5050
-
-int n;
-ll a[MAX_N], t;
-vector<ll> sum;
+ll L, R, T;
 
 int main(void) {
 	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-//	freopen(".inp", "r", stdin);
-//	freopen(".out", "w", stdout);
 
-	cin >> n >> t;
-	t *= 3;
-	FOR(i, 1, n) {
-		cin >> a[i];
-	}
+	cin >> L >> R >> T;
 
-	FOR(i, 1, n-1) {
-		FOR(j, i+1, n) {
-			sum.push_back(a[i] + a[j]);
-		}
-	}
+	ll xmax = T / 2 - (T % 2 == 0), xmin = max(L, T - R);
 
-	sort(a+1, a+n+1);
+	if (xmax >= T - xmax) xmax--;
 
-	ll minC = 1e18, ans = -1e18;
+	if (xmax > R || xmin >= T - xmin) return cout << 0, 0;
 
-	FOR(i, 0, sz(sum)-1) {
-		int mi = lower_bound(a+1, a+n+1, t - sum[i]) - a;
-		int mx = upper_bound(a+1, a+n+1, t - sum[i]) - a - 1;
-		if (abs(sum[i] + a[mi] - t) < minC) {
-			minC = abs(sum[i] + a[mi] - t);
-			cerr << a[mi] << " " << sum[i] << "\n";
-			ans = max(ans, sum[i] + a[mi]);
-		}
-		if (abs(sum[i] + a[mx] - t) < minC) {
-			minC = abs(sum[i] + a[mx] - t);
-			cerr << a[mx] << " " << sum[i] << "\n";
-			ans = max(ans, sum[i] + a[mx]);
-		}
-	}
-
-	cout << ans;
+	cout << max(0LL, xmax - xmin + 1);
 
 	return 0;
 }
--- TASK/NVT_brute.cpp
+++ TASK/NVT_brute.cpp
@@ -16,49 +16,27 @@ template <class X, class Y> bool umax(X &a, const Y &b) { return a < b ? a = b, 
 template <class T> string to_str(const T &a, int p = -1) { stringstream ss; p >= 0 ? ss << fixed << setprecision(p) << a : ss << a; return ss.str(); }
 template <class T> T abs(const T &a) { return a >= 0 ? a : -a; }
 
-#define MAX_N	5050
-
-int n;
-ll a[MAX_N], t;
-vector<ll> sum;
+ll L, R, T;
 
 int main(void) {
 	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-//	freopen(".inp", "r", stdin);
-//	freopen(".out", "w", stdout);
+	freopen("NVT.inp", "r", stdin);
+	freopen("NVT.ans", "w", stdout);
 
-	cin >> n >> t;
-	t *= 3;
-	FOR(i, 1, n) {
-		cin >> a[i];
+	cin >> L >> R >> T;
+
+	cerr << L << " " << R << " " << T << "\n";
+
+	ll cnt = 0;
+
+	for(ll i = L; i <= R; ++i) if (i < T - i && T - i <= R) {
+		cerr << i << " ";
+		cnt++;
 	}
 
-	FOR(i, 1, n-1) {
-		FOR(j, i+1, n) {
-			sum.push_back(a[i] + a[j]);
-		}
-	}
+	cout << cnt;
 
-	sort(a+1, a+n+1);
-
-	ll minC = 1e18, ans = -1e18;
-
-	FOR(i, 0, sz(sum)-1) {
-		int mi = lower_bound(a+1, a+n+1, t - sum[i]) - a;
-		int mx = upper_bound(a+1, a+n+1, t - sum[i]) - a - 1;
-		if (abs(sum[i] + a[mi] - t) < minC) {
-			minC = abs(sum[i] + a[mi] - t);
-			cerr << a[mi] << " " << sum[i] << "\n";
-			ans = max(ans, sum[i] + a[mi]);
-		}
-		if (abs(sum[i] + a[mx] - t) < minC) {
-			minC = abs(sum[i] + a[mx] - t);
-			cerr << a[mx] << " " << sum[i] << "\n";
-			ans = max(ans, sum[i] + a[mx]);
-		}
-	}
-
-	cout << ans;
+//	cerr << cnt << "\n";
 
 	return 0;
 }
