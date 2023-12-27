c1586fb10b2bc1a4d5e14fd6c32d17a341f0600c
Tinh Ngo <tinhngo.ptho@gmail.com>
2023-12-27 21:19:37 +0700

    ADD 27/12/2023

--- EducationCode/matrix.cpp
+++ EducationCode/matrix.cpp
@@ -17,11 +17,14 @@ template<class X, class Y> bool umin(X &a, const Y &b) { return a > b ? a = b, 1
 template<class T> string to_str(const T &a, int p = -1) { stringstream ss; p >= 0 ? ss << fixed << setprecision(p) << a : ss << a; return ss.str(); }
 template<class T> T abs(const T &a) { return a >= 0 ? a : -a; }
 
+struct matrix {
+
+};
 
 int main(void) {
 	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 //	freopen(".inp", "r", stdin);
 //	freopen(".out", "w", stdout);
 
-	|
+
 }
